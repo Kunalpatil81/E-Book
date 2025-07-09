8e4f91c454ad62d1babf2d08163cf7c4975da813
Kunalpatil <kunalpatil1883@gmail.com>
2025-07-09 13:36:50 +0530

    upload files to cloudinary

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,9 +1,51 @@
+import path from "node:path";
 import { asyncHandler } from "../utils/asyncHandler";
 import { Request, Response, NextFunction } from "express";
+import cloudinary from "../config/cloudinary";
 
 const createBook = asyncHandler(
     async (req: Request, res: Response, next: NextFunction) => {
-        const {} = req.body;
+        const files = req.files as {
+            [fieldname: string]: Express.Multer.File[];
+        };
+        // 'application/pdf'
+        const coverImageMimeType = files.coverImage[0].mimetype
+            .split("/")
+            .at(-1);
+        const fileName = files.coverImage[0].filename;
+        const filePath = path.resolve(
+            __dirname,
+            "../../public/data/uploads",
+            fileName
+        );
+
+        const uploadResult = await cloudinary.uploader.upload(filePath, {
+            filename_override: fileName,
+            folder: "book-covers",
+            format: coverImageMimeType,
+        });
+
+        const bookFileName = files.file[0].filename;
+        const bookFilePath = path.resolve(
+            __dirname,
+            "../../public/data/uploads",
+            bookFileName
+        );
+
+        const bookFilePathUploadResult = await cloudinary.uploader.upload(
+            bookFilePath,
+            {
+                resource_type: "raw",
+                filename_override: bookFileName,
+                folder: "book-pdfs",
+                format: "pdf",
+            }
+        );
+
+        console.log('bookFileResult', bookFilePathUploadResult)
+
+        console.log("uploadresults", uploadResult);
+        res.json({});
     }
 );
 
--- src/config/config.ts
+++ src/config/config.ts
@@ -1,11 +1,15 @@
 import { config as conf } from "dotenv";
+
 conf();
 
 const _config = {
     port: process.env.PORT,
     MONGO_URI: process.env.MONGO_URI,
     env: process.env.NODE_ENV,
-    jwtSecret: process.env.JWT_SECRET
+    jwtSecret: process.env.JWT_SECRET,
+    cloudinaryCloudName: process.env.CLOUDINARY_CLOUD_NAME,
+    cloudinaryApi: process.env.CLOUDINARY_API_KEY,
+    cloudinarySecret: process.env.CLOUDINARY_SECRET,
 };
 
 export const config = Object.freeze(_config);
